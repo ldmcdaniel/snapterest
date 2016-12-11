30279810c7a172faf533a7bcc2c26cfa96fa12fd
Lucas McDaniel <ldmcdaniel@gmail.com>
2016-12-11 11:15:48 -0600

    Playing with React Elements

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var reactElement = React.createElement('h1');
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+
+var h1 = React.createElement('h1', {className: 'header', key: 'header'}, 'This is React');
+var p = React.createElement('p', {className: 'content', key: 'content'}, 'And this is how it works');
+var reactFragment = [h1, p];
+var section = React.createElement('section', {className: 'container'}, reactFragment);
+
+ReactDOM.render(section, document.getElementById('react-application'));
