635b0bfebb487ec8449953cd46910890133ac964
Lucas McDaniel <ldmcdaniel@gmail.com>
2016-12-18 16:09:51 -0600

    Added StreamTweet component

---
+++ src/components/StreamTweet.react.js
@@ -0,0 +1,44 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Header = require('./Header.react');
+var Tweet = require('./Tweet.react');
+
+var StreamTweet = React.createClass({
+  getInitialState: function () {
+    console.log('[Snapterest] StreamTweet: 1. Running getInitialState()');
+
+    return {
+      numberOfCharactersIsIncreasing: null,
+      headerText: null
+    };
+  },
+  componenetWillMount: function () {
+    console.log('[Snapterest] StreamTweet: 2. Running componenetWillMount()');
+    this.setState({
+      numberOfCharactersIsIncreasing: true,
+      headerText: 'Latest public photo from Twitter'
+    });
+    window.snapterest = {
+      numberOfReceivedTweets: 1,
+      numberofDisplayedTweets: 1
+    };
+  },
+  componentDidMount: function () {
+    console.log('[Snapterest] StreamTweet: 3. Running componentDidMount()');
+    var componentDOMRepresentation = ReactDOM.findNode(this);
+    window.snapterest.headerHtml = componentDOMRepresentation.children[0].outerHTML;
+    window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
+  },
+  render: function () {
+    console.log('[Snapterest] StreamTweet: Running render()');
+
+    return (
+      <section>
+        <Header text={this.state.headerText} />
+          <Tweet tweet={this.props.tweet} onImageClick={this.props.onAddTweetToCollection} />
+      </section>
+    );
+  }
+});
+
+module.exports = StreamTweet;
